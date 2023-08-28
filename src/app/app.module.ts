9037e59159613093b89482f1114d928d236ada2d
Rofiki Harong <vidocq.hrg@gmail.com>
2023-08-28 15:02:12 +0700

    install ngx-bootstrap , fortawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonsModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    FontAwesomeModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
