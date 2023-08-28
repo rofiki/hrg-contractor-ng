ffd209f71367c0be67354bf3b9d05acd5ae95186
Rofiki Harong <vidocq.hrg@gmail.com>
2023-08-28 12:05:57 +0700

    reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { MeInfoComponent } from './me-info/me-info.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent,
-    MeInfoComponent,
-    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
